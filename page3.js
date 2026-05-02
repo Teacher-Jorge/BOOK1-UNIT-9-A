5f4c2f85996f7735b277b706e7a21f20ea41b89f
Teacher-Jorge <79639664+Teacher-Jorge@users.noreply.github.com>
2026-05-02 08:51:47 -0300

    Add files via upload

---
+++ page3.js
@@ -0,0 +1,47 @@
+
+const play = document.getElementById('play')
+
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const input3 = document.getElementById('input3')
+
+play.addEventListener('click', function() {
+    const video = document.getElementById('video').play()
+
+})
+
+input1.addEventListener('keyup', (e) => {
+    if(input1.value === "Can") {
+        input1.style.color = 'green'
+        const audio = document.getElementById('right')
+        audio.play()
+        document.getElementById('input2').focus()
+    }
+})
+
+input2.addEventListener('keyup', (e) => {
+    if(input2.value === "sit") {
+        input2.style.color = 'green'
+        const audio = document.getElementById('right')
+        audio.play()
+        document.getElementById('input3').focus()
+    }
+})
+
+input3.addEventListener('keyup', (e) => {
+    if(input3.value === "can") {
+        input3.style.color = 'green'
+        const audio = document.getElementById('right')
+        audio.play()
+        const next = document.getElementById('next')
+        next.style.visibility = 'visible'
+    }
+})
+
+
+
+
+
+
+
+
---
+++ page5.js
@@ -0,0 +1,59 @@
+
+const play = document.getElementById('play')
+
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const input3 = document.getElementById('input3')
+const input4 = document.getElementById('input4')
+
+play.addEventListener('click', function() {
+    const video = document.getElementById('video').play()
+
+})
+
+input1.addEventListener('keyup', (e) => {
+    if(input1.value === "Can") {
+        input1.style.color = 'green'
+        const audio = document.getElementById('right')
+        audio.play()
+        document.getElementById('input2').focus()
+    }
+})
+
+input2.addEventListener('keyup', (e) => {
+    if(input2.value === "go" || input2.value === "go") {
+        input2.style.color = 'green'
+        const audio = document.getElementById('right')
+        audio.play()
+        document.getElementById('input3').focus()
+    }
+})
+
+input3.addEventListener('keyup', (e) => {
+    if(input3.value === "No" || input3.value === "Yes") {
+        input3.style.color = 'green'
+        const audio = document.getElementById('right')
+        audio.play()
+        
+        document.getElementById('input4').focus()
+    }
+})
+
+input4.addEventListener('keyup', (e) => {
+    if(input4.value === "can" || input4.value === "can't" || input4.value === "cant") {
+        input4.style.color = 'green'
+        const audio = document.getElementById('right')
+        audio.play()
+        const next = document.getElementById('next')
+        next.style.visibility = 'visible'
+    }
+})
+
+
+
+
+
+
+
+
+
